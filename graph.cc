70ede9bb76f4418ed032fb042613d87d884a67ca
Richard Strong Bowen <rsbowen@gmail.com>
2016-09-05 10:02:40 -0700

    Convenience operators for Word.

--- graph.cc
+++ graph.cc
@@ -1,7 +1,11 @@
-#include <unordered_set>
+#include <algorithm>
+#include <limits>
 #include <string>
 
 #include "graph.h"
+
+// tmp
+#include <iostream>
 
 std::vector<std::vector<Word>> ConnectedComponents(const PuzzleGraph& graph) {
   std::vector<std::vector<Word>> connected_components;
@@ -27,4 +31,78 @@ std::vector<std::vector<Word>> ConnectedComponents(const PuzzleGraph& graph) {
     }
   }
   return connected_components;
+}
+
+// TODO: break this into 3 functions which can be tested individually
+std::vector<std::unordered_set<Word>> FillOrder(Puzzle puzzle) {
+  std::vector<std::unordered_set<Word>> fill_order;
+  for(PuzzleGraph graph = puzzle.AsGraph(); !graph.empty(); graph=puzzle.AsGraph()) {
+    std::unordered_set<Word> word_set;
+    fill_order.emplace_back();
+    const auto connected_components = ConnectedComponents(graph);
+    int min_size = std::numeric_limits<int>::max(), index;
+    for(int i = 0; i<connected_components.size(); ++i) {
+      if(connected_components[i].size() < min_size) {
+        min_size = connected_components[i].size();
+	index = i;
+      }
+    }
+    if(min_size <= 6) {
+      for(const auto& word : connected_components[index]) {
+        // TODO: make puzzle take Word arguments
+        std::string word_already = puzzle.WordAt(word.coords_, word.direction_);
+	for(char& c : word_already) c='x';
+	puzzle.SetWord(word.coords_, word.direction_, word_already);
+	fill_order.back().insert(word);
+      }
+      continue;
+    }
+
+    // See if there's a word which, if filled, will break into more connected components.
+    int max_num_connected_components = connected_components.size(), connected_components_score = std::numeric_limits<int>::max();
+    Word location;
+    
+    for(PuzzleIterator iter(puzzle); iter.Valid(); iter.Increment()) {
+      // TODO: this is probably wildly inefficient, can be smarter about how tracking connected components to not redo this calculation every time.
+      Puzzle sub_puzzle = puzzle; 
+      // TODO: change Word to something else or make it so you can use it like a string (yikes...)
+      Word word = iter.Get();
+      std::string word_already = sub_puzzle.WordAt(word.coords_, word.direction_);
+      if(std::find(word_already.begin(), word_already.end(), ' ') == word_already.end()) continue;
+      for(char& c : word_already) c='x';
+      sub_puzzle.SetWord(word.coords_, word.direction_, word_already);
+      auto new_connected_components = ConnectedComponents(sub_puzzle.AsGraph());
+      if(new_connected_components.size() > max_num_connected_components) {
+        int sub_cc_score = 0;
+	for(const auto& cc : new_connected_components) { sub_cc_score += (cc.size())*(cc.size()); }
+	if(sub_cc_score < connected_components_score) {
+	  max_num_connected_components = new_connected_components.size();
+	  connected_components_score = sub_cc_score;
+	  location = word;
+	}
+      }
+    }
+    if(max_num_connected_components > connected_components.size()) {
+      std::string word_already = puzzle.WordAt(location.coords_, location.direction_);
+      for(char& c : word_already) c='x';
+      puzzle.SetWord(location.coords_, location.direction_, word_already);
+      fill_order.back().insert(location);
+      continue;
+    }
+
+    int max_num_blanks = 0;
+    for(PuzzleIterator iter(puzzle); iter.Valid(); iter.Increment()) {
+      std::string word = puzzle.WordAt(iter.Get().coords_, iter.Get().direction_);
+      int blanks = std::count(word.begin(), word.end(), ' ');
+      if(blanks > max_num_blanks) { 
+        max_num_blanks = blanks;
+	location = iter.Get();
+      }
+    }
+    std::string word_already = puzzle.WordAt(location.coords_, location.direction_);
+    for(char& c : word_already) c='x';
+    puzzle.SetWord(location.coords_, location.direction_, word_already);
+    fill_order.back().insert(location);
+  }
+  return fill_order;
 }
--- graph.h
+++ graph.h
@@ -2,8 +2,17 @@
 #define GRAPH_H
 
 #include "puzzle.h"
+#include <unordered_set>
 // TODO: move puzzlegraph definition here.
 
 std::vector<std::vector<Word>> ConnectedComponents(const PuzzleGraph& graph);
+
+// Strategy:
+//  1) If the smallest connected component has 6 or fewer words, fill it (all at once)
+//  2) If filling any word would increase the number of connected components, fill it.
+//  3) Fill the word with the most blanks
+//
+// Output: list (in order) of sets (not in order) of words to be filled.
+std::vector<std::unordered_set<Word>> FillOrder(Puzzle puzzle);
 
 #endif
